d2e254e6db461972f3603ba1bf85e2109c23ad55
Steve Henty <stevehenty@gmail.com>
2019-05-28 14:17:48 +0200

    Use strict locator

--- tests/acceptance/testWordPressCept.php
+++ tests/acceptance/testWordPressCept.php
@@ -9,6 +9,7 @@ $I->amOnUrl( 'https://wordpress.org' );
 $I->fillField('search', 'henty');
 $I->click('Submit');
 $I->seeCurrentURLEquals('/search/henty');
-$I->click('WordPress › Profiles » Steve Henty');
+$I->waitForText('WordPress › Profiles » Steve Henty');
+$I->click(['link' => 'WordPress › Profiles » Steve Henty'] );
 $I->seeCurrentURLEquals('/stevehenty/');
 $I->see('WordCamp Speaker');
