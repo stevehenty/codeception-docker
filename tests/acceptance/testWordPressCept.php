0de94be3b4dd12e3bd60f3db34d171797faee3d3
Steve Henty <stevehenty@gmail.com>
2019-08-24 11:12:44 +0200

    Update testWordPressCept.php

--- tests/acceptance/testWordPressCept.php
+++ tests/acceptance/testWordPressCept.php
@@ -6,10 +6,10 @@ $I->wantTo( 'Test that WordPress works.' );
 
 $I->amOnUrl( 'https://wordpress.org' );
 
-$I->fillField('search', 'henty');
-$I->click('Submit');
-$I->seeCurrentURLEquals('/search/henty');
-$I->waitForText('WordPress › Profiles » Steve Henty');
-$I->click(['link' => 'WordPress › Profiles » Steve Henty'] );
-$I->seeCurrentURLEquals('/stevehenty/');
-$I->see('WordCamp Speaker');
+$I->fillField( 'search', 'henty' );
+$I->click( 'Submit' );
+$I->seeCurrentURLEquals( '/search/henty' );
+$I->waitForText( 'Steve Henty – @stevehenty | WordPress.org' );
+$I->click( [ 'link' => 'Steve Henty – @stevehenty | WordPress.org' ] );
+$I->seeCurrentURLEquals( '/stevehenty/' );
+$I->see( 'WordCamp Speaker' );
